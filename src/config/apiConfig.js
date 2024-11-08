57a28d50a940d6680244a3a90a96c2d0026370bd
akai1881 <akaibegaliev1881@gmail.com>
2024-11-08 16:22:32 +0600

    added explanation commnets

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,1 +1,1 @@
-export const API_URL = import.meta.env.VITE_BASE_API_URL || 'https://randomuser.me';
+export const API_URL = import.meta.env.VITE_BASE_API_URL || 'https://randomuser.me'; // for fast testing added url as string, normally should be saved in ENV
